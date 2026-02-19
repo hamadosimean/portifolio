84b875e81508995bcc676f8a5a30216194bfd3e4
Hamado Simean <hamadosimeam226@gmail.com>
2026-02-19 14:53:55 +0000

    refactor photo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center h-16 bg-slate-900/20 backdrop-blur-md border-t border-slate-800">
-      <p className="text-gray-300 text-5xl md:text-7xl font-extrabold">
+    <footer className="py-10 space-y-4 flex flex-col items-center justify-center  bg-slate-900/20 backdrop-blur-md border-t border-slate-800">
+      <p className="text-gray-300 text-7xl md:text-9xl font-extrabold">
         Software Engineer
       </p>
-      <p className="text-gray-300 text-sm">
+      <p className="text-gray-300 text-xl">
         Hamado Simean {new Date().getFullYear()} &copy; All rights reserved.
       </p>
     </footer>
