b98d186c9e9e562a25437cc50bb7037daad2a70f
Hamado Simean <hamadosimeam226@gmail.com>
2026-02-19 14:36:31 +0000

    refactor photo

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,7 +9,7 @@ function About() {
         <img
           src={image}
           alt="myimage"
-          className="mx-auto my-4 rounded-md min-w-full min-h-[400px] object-contain border border-slate-700 hover:border-blue-500 transition-colors duration-300 hover:shadow-blue-500 hover:cursor-pointer"
+          className="mx-auto my-4 rounded-md h-auto w-full object-contain border border-slate-700 hover:border-blue-500 transition-colors duration-300 hover:shadow-blue-500 hover:cursor-pointer"
         />
         {/* Bio Section */}
         <section className="bg-slate-800/50 p-8 rounded-2xl border border-slate-700">
