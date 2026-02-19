f7abbc15119c3761fc6aef89bd91e0730abace40
Hamado Simean <hamadosimeam226@gmail.com>
2026-02-19 14:57:44 +0000

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <footer className="py-10 space-y-4 flex flex-col items-center justify-center  bg-slate-900/20 backdrop-blur-md border-t border-slate-800">
-      <p className="text-gray-300 text-5xl sm:text-7xl md:text-9xl font-extrabold text-center">
+      <p className="text-center text-gray-300 text-5xl sm:text-7xl md:text-9xl font-extrabold">
         Software Engineer
       </p>
-      <p className="text-gray-300 text-xl sm:text-2xl md:text-3xl">
+      <p className="text-center text-gray-300 text-xl sm:text-2xl md:text-3xl">
         Hamado Simean {new Date().getFullYear()} &copy; All rights reserved.
       </p>
     </footer>
